yunwoo-yu/front_6th_chapter4-1

feat: 프로젝트 초기설정 완료

packages/vanilla/src/constants.js
@@ -1,0 +1,1 @@
+export const BASE_URL = import.meta.env.PROD ? "/front_6th_chapter4-1/vanilla/" : "/";

junijaei/front_7th_chapter4-1

packages/vanilla/src/constants.js
@@ -1,0 +1,1 @@
+export const BASE_URL = import.meta.env.PROD ? "/front_7th_chapter4-1/vanilla/" : "/";